4731ebb8c6eda6de3d8e30e660c9195a8c621e8a
MacBook Pro <naym100m@gmail.com>
2024-03-21 19:09:47 +0600

    create a sidebar with components

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="px-5 py-3">
       <body className={inter.className}>
         <Sidebar />
         <div className="min-h-screen">{children}</div>
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { Button, Drawer, Space } from "antd";
 import { VscThreeBars } from "react-icons/vsc";
+import Link from "next/link";
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
   const [placement, setPlacement] = useState("left");
@@ -14,24 +15,67 @@ const Sidebar = () => {
   const onChange = (e) => {
     setPlacement(e.target.value);
   };
+
+  const navLinks = [
+    {
+      route: "Home",
+      pathname: "/",
+    },
+    {
+      route: "Discussion Forum",
+      pathname: "/discussionForum",
+    },
+    {
+      route: "Market Stories",
+      pathname: "/marketStories",
+    },
+    {
+      route: "Sentiment",
+      pathname: "/sentiment",
+    },
+    {
+      route: "Market",
+      pathname: "/market",
+    },
+    {
+      route: "Sector",
+      pathname: "/sector",
+    },
+    {
+      route: "Watch list",
+      pathname: "/watchList",
+    },
+    {
+      route: "Events",
+      pathname: "/events",
+    },
+    {
+      route: "News/Interview",
+      pathname: "/newsInterview",
+    },
+  ];
+
   return (
     <>
       <Space>
-        <Button type="primary" onClick={showDrawer}>
-          <VscThreeBars />
+        <Button onClick={showDrawer}>
+          <VscThreeBars className=" text-xl" />
         </Button>
       </Space>
       <Drawer
-        title="Basic Drawer"
+        title="Hello, User"
         placement={placement}
-        closable={false}
         onClose={onClose}
         open={open}
         key={placement}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        {navLinks.map((item) => (
+          <Link key={item} href={item.pathname}>
+            <div className="text-black font-bold grid mt-4 text-start border py-2 px-5 rounded-md bg-slate-50">
+              {item.route}
+            </div>
+          </Link>
+        ))}
       </Drawer>
     </>
   );

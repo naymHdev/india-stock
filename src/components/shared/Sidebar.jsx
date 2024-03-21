3f61a88e847426da41c625348e2508d851888c72
MacBook Pro <naym100m@gmail.com>
2024-03-21 21:02:22 +0600

    updated a sidebar

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,11 +1,14 @@
 "use client";
 import React, { useState } from "react";
-import { Button, Drawer, Space } from "antd";
+import { Drawer, Space } from "antd";
 import { VscThreeBars } from "react-icons/vsc";
+import { IoMdNotificationsOutline } from "react-icons/io";
 import Link from "next/link";
+
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
   const [placement, setPlacement] = useState("left");
+
   const showDrawer = () => {
     setOpen(true);
   };
@@ -58,20 +61,29 @@ const Sidebar = () => {
   return (
     <>
       <Space>
-        <Button onClick={showDrawer}>
-          <VscThreeBars className=" text-xl" />
-        </Button>
+        <div className="bg-[#0A1237] px-4 py-2 rounded-md hover:cursor-pointer" onClick={showDrawer}>
+          <VscThreeBars className=" text-xl text-white" />
+        </div>
       </Space>
       <Drawer
-        title="Hello, User"
+      onClose={onClose}
         placement={placement}
-        onClose={onClose}
         open={open}
         key={placement}
+        extra={
+          <Space>
+            <h2 className=" text-xl text-white font-semibold -ml-[280px]">
+              Hello, User
+            </h2>
+            <IoMdNotificationsOutline className="text-3xl hover:cursor-pointer text-white" />
+          </Space>
+        }
+        style={{ background: "#0B1337" }}
       >
+        <hr />
         {navLinks.map((item) => (
           <Link key={item} href={item.pathname}>
-            <div className="text-black font-bold grid mt-4 text-start border py-2 px-5 rounded-md bg-slate-50">
+            <div className="font-bold grid text-start hover:shadow-xl py-3 px-5 rounded-sm text-white">
               {item.route}
             </div>
           </Link>
